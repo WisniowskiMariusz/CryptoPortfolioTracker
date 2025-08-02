b2c86724f6598e443381c44f206b65a3f3a623d0
Mariusz <mariuszwisniowski@gmail.com>
2025-08-02 19:12:38 +0200

    fixing tests after binance_service refactor

--- app/tools.py
+++ app/tools.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 from fastapi import HTTPException
 
@@ -31,4 +31,33 @@ def timestamp_from_str(date_str: str | None) -> int | None:
 def datetime_from_miliseconds(miliseconds: int | None) -> datetime | None:
     if not miliseconds:
         return None
-    return datetime.fromtimestamp(miliseconds / 1000)
+    if miliseconds < 0:
+        return datetime.min.replace(tzinfo=timezone.utc)
+    return datetime.fromtimestamp(miliseconds / 1000, tz=timezone.utc)
+
+
+def convert_time_to_ms(time: str) -> int:
+    print(f"Provided time string: {time}")
+    if not isinstance(time, datetime):
+        dt = datetime.strptime(time, "%Y-%m-%d %H:%M")
+    else:
+        dt = time
+    dt = dt.replace(tzinfo=timezone.utc)
+    now = datetime.now(timezone.utc)
+    print(f"Provided time: {dt}")
+    print(f"Current time: {now}")
+    if dt > now:
+        dt = now  # Think about raising error here
+    print(f"Used time: {dt}")
+    timestamp_ms = int(dt.timestamp() * 1000)
+    return timestamp_ms
+
+
+def add_n_days_to_date(
+    days: int, date: datetime = datetime.now(timezone.utc)
+) -> datetime:
+    """
+    Move the date forward or back (if days number is negative)
+    by a specified number of days. Default is now.
+    """
+    return date + timedelta(days=days)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import os
 import pytest
 import keyring
+from unittest.mock import patch, MagicMock
+from app.binance_service import BinanceService
 
 os.environ["USE_SQL_SERVER"] = "false"
 
@@ -8,10 +10,35 @@ os.environ["USE_SQL_SERVER"] = "false"
 @pytest.fixture(autouse=True)
 def mock_keyring(monkeypatch):
     def fake_get_password(service_name, username):
+        if service_name == "only_api_key_correct":
+            if username == "api_secret":
+                raise keyring.errors.KeyringError
+        elif service_name != "fake keyring system name":            
+            raise keyring.errors.KeyringError
         if username == "api_key":
             return "fake_api_key"
         elif username == "api_secret":
             return "fake_api_secret"
-        return None
-
     monkeypatch.setattr(keyring, "get_password", fake_get_password)
+
+
+@pytest.fixture
+def mocked_binance_service_instance():
+    with patch("app.main.binance_service") as mocked_bs_instance:
+        yield mocked_bs_instance
+
+
+@pytest.fixture
+def mocked_binance_service():
+    mock_client = MagicMock()
+
+    # This patch replaces the Spot class with your mock
+    with patch("app.binance_service.Spot", return_value=mock_client):
+        # Create the service with fake credentials (from mock_keyring)
+        service = BinanceService("fake keyring system name")
+        yield service, mock_client
+
+
+@pytest.fixture
+def fake_binance_service(mocked_binance_service):
+    return mocked_binance_service[0]
